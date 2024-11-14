1aa6c9b4791f59ed9f0fb6bd377f216a97a0f434
Quick <quick0831@gmail.com>
2024-11-14 23:18:29 +0800

    update .ioc from 2022 to 2024

---
+++ Core/Inc/app_main.h
@@ -0,0 +1,15 @@
+#ifndef __APP_MAIN_H
+#define __APP_MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int app_main(void);
+void Interrupt_Handle();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */

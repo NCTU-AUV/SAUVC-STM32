dff134c46c88dd9a2941245f6fd48300a5518652
DianYueGuo <dianyueguo@gmail.com>
2024-11-24 02:38:44 +0800

    change TIM2 and TIM8's prescaler to 16-1 so that the timer would be 1 MHz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -265,7 +265,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 84-1;
+  htim2.Init.Prescaler = 16-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 3000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -337,7 +337,7 @@ static void MX_TIM8_Init(void)
 
   /* USER CODE END TIM8_Init 1 */
   htim8.Instance = TIM8;
-  htim8.Init.Prescaler = 168-1;
+  htim8.Init.Prescaler = 16-1;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim8.Init.Period = 3000-1;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;

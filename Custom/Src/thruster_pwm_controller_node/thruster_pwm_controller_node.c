aab9c10d40446ec5e2da7d8d26a9523d2812a11b
DianYueGuo <dianyueguo@gmail.com>
2025-12-26 22:17:01 +0800

    Remove the pwm_output_signal_value_us publisher entirely

--- Custom/Src/thruster_pwm_controller_node/thruster_pwm_controller_node.c
+++ Custom/Src/thruster_pwm_controller_node/thruster_pwm_controller_node.c
@@ -1,7 +1,6 @@
 #include "thruster_pwm_controller_node/thruster_pwm_controller_node.h"
 
 #include "thruster_pwm_controller_node/subscribe_set_pwm_output_on.h"
-#include "thruster_pwm_controller_node/publish_pwm_output_signal_value.h"
 #include "thruster_pwm_controller_node/subscribe_set_pwm_output_signal_value.h"
 
 const unsigned int THRUSTER_PWM_CONTROLLER_NUM_HANDLES = 8 + 8;
@@ -10,14 +9,6 @@ const unsigned int THRUSTER_PWM_CONTROLLER_NUM_HANDLES = 8 + 8;
 
 static void thruster_pwm_controller_timer_callback(rcl_timer_t *, int64_t)
 {
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER0);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER1);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER2);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER3);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER4);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER5);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER6);
-    publish_pwm_output_signal_value_only_when_updated(THRUSTER7);
 }
 
 
@@ -29,11 +20,6 @@ void thruster_pwm_controller_on_timer_tick(void)
 
 void initialize_thruster_pwm_controller_node(rclc_support_t *support, rclc_executor_t *executor, rcl_node_t *stm32_node)
 {
-    if (!initialize_pwm_output_signal_value_publisher_for_all_thrusters(stm32_node)) {
-        printf("thruster_pwm_controller_node: aborting init; failed to set up pwm_output_signal_value publishers\n");
-        return;
-    }
-
     if (!initialize_set_pwm_output_on_subscriber_for_all_thrusters(stm32_node, executor)) {
         printf("thruster_pwm_controller_node: aborting init; failed to set up set_pwm_output_on subscribers\n");
         return;
@@ -42,6 +28,4 @@ void initialize_thruster_pwm_controller_node(rclc_support_t *support, rclc_execu
         printf("thruster_pwm_controller_node: aborting init; failed to set up set_pwm_output_signal_value subscribers\n");
         return;
     }
-
-    initialize_previous_pwm_output_signal_value_for_all_thrusters();
 }
--- Makefile
+++ Makefile
@@ -181,7 +181,6 @@ C_SOURCES += Custom/Src/MS5837.c
 C_SOURCES += Custom/Src/kill_switch_node.c
 C_SOURCES += Custom/Src/thruster_pwm_controller_node/thruster_pwm_controller_node.c
 C_SOURCES += Custom/Src/thruster_pwm_controller_node/subscribe_set_pwm_output_on.c
-C_SOURCES += Custom/Src/thruster_pwm_controller_node/publish_pwm_output_signal_value.c
 C_SOURCES += Custom/Src/thruster_pwm_controller_node/subscribe_set_pwm_output_signal_value.c
 C_SOURCES += Custom/Src/pressure_sensor_node.c
 C_SOURCES += Custom/Src/debug_logger.c
